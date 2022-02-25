be48c47bee49029aa41d8fa9a3fa19d1baef08eb
Mikah Mathews <mikah@outofband.org>
2022-02-24 16:17:31 -0800

    WIP: Start adding terminal logic, replacement method
    Co-authored-by: Brenna Lavin <lavinbrenna@gmail.com>

---
+++ FindNReplace/Models/Replacement.cs
@@ -0,0 +1,7 @@
+namespace FindNReplace.Models
+{
+  public class Replacement
+  {
+
+  }
+}
---
+++ FindNReplace/Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace FindNReplace.Models
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Please write a sentence");
+      string sentence = Console.ReadLine();
+      Console.WriteLine("Please choose a word from your sentence to replace");
+      string string1 = Console.Readline();
+      Console.WriteLine("Pleace choose the word you'd like to replace it with");
+      Replacement newReplacement = new Replacement();
+      Replacement.
+    }
+  }
+}
+
